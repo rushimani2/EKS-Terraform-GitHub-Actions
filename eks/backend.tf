61337b2e2e5744a0337ac4bffc91e1548cdd8bd8
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2024-08-13 04:02:41 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket812"
+    bucket         = "my-ews-baket811"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
